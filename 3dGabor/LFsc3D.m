0f375ed351473b42dce153d2f39c3bc5e84a0159
Avner Lavi <avnerlavi308@gmail.com>
2021-08-28 19:46:58 +0300

    flipped xy/t ratio in LF Gaussian width

--- 3dGabor/LFsc3D.m
+++ 3dGabor/LFsc3D.m
@@ -2,7 +2,7 @@ function [out, vidNR] = LFsc3D(vid,Az,El,Fac)
 sigmaNR = sqrt(0.1);
 vidNR = NakaRushton(vid, sigmaNR, 0, 2);
 %g0 = Gaussian3D([Az,El],0,[0.01,0.01,Fac],[]); %LF without width
-g0 = Gaussian3D([Az,El],0,[0.05,0.05,1]*Fac,[1.5, 1.5, 1.5]*Fac); %with width
+g0 = Gaussian3D([Az,El],0,[1,1,0.05]*(20/Fac),[1.5, 1.5, 1.5]*Fac); %with width
 g0_normed = g0/max(g0(:));
 out = convn(vidNR,g0_normed,'same');
 end
