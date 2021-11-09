4950afad1a787dad5ee85f5ccaae3cbd90e6c600
Galalon <sgalalon@gmail.com>
2021-11-09 22:00:10 +0200

    added orientation response normalization

--- 3dGabor/Gabor3DActivation.m
+++ 3dGabor/Gabor3DActivation.m
@@ -1,10 +1,4 @@
-function [LF_n, LF_p] = Gabor3DActivation(vidS, Azimuth, Elevation, activationThreshold, FacilitationLength, alpha)
-L = BuildGabor3D(Azimuth, Elevation);
-Co = convn(vidS, L, 'same');
-%threshold = activationThreshold * max(abs(Co(8:end-7,8:end-7,8:end-7)), [], 'all');
-%Co(abs(Co) < threshold) = 0;
-Cp = max( Co,0);
-Cn = max(-Co,0);
+function [LF_n, LF_p] = Gabor3DActivation(Cp,Cn, Azimuth, Elevation, activationThreshold, FacilitationLength, alpha)
 
 %Cp(Cp < threshold_p) = 0;
 %Cn(Cn < threshold_n) = 0;
@@ -18,7 +12,7 @@ threshold_n = activationThreshold * max(LF_n(8:end-7,8:end-7,8:end-7), [], 'all'
 %threshold_p = prctile(LF_p(8:end-7,8:end-7,8:end-7),100*(activationThreshold),'all');
 %threshold_n = prctile(LF_n(8:end-7,8:end-7,8:end-7),100*(activationThreshold),'all');
 
-disp(mean(double(abs(LF_n) < threshold_n),'all'))%debug for data proccesing
+%disp(mean(double(abs(LF_n) < threshold_n),'all'))%debug for data proccesing
 LF_n(abs(LF_n) < threshold_n) = NR_n(abs(LF_n) < threshold_n);
 LF_p(abs(LF_p) < threshold_p) = NR_p(abs(LF_p) < threshold_p);
 
--- 3dGabor/computeCombinedLF_IN3D.m
+++ 3dGabor/computeCombinedLF_IN3D.m
@@ -1,12 +1,11 @@
-function [vidScaleTot, vidScalesPyr] = computeCombinedLF_IN3D(vidIn, nAzimuths, ...
-    nElevations, elHalfAngle, eccentricity, nScales, activationThreshold, ...
-    baseFacilitationLengths, alpha, m1, m2)
-%waitbar handling
+function [vidScaleTot, vidScalesPyr] = computeCombinedLF_IN3D(vidIn, nAzimuths ...
+    , nElevations, elHalfAngle, eccentricity, nScales, activationThreshold, baseFacilitationLengths ...
+    , alpha, m1, m2, normQ)
+
 w = waitbar(0, 'starting per-resolution LF computation');
 progressCounter = 0;
-totalIterationNumber = nScales * nElevations * nAzimuths + nScales;
-%input handling
-vidIn = PadVideoReplicate(vidIn,2*nScales); %adding margin to avoid edge effects
+vidIn = PadVideoReplicate(vidIn,2*nScales);
+
 vidScaleTot = zeros(size(vidIn));
 vidScalesPyr = cell(nScales);
 %halfAngle parsing
@@ -47,35 +46,75 @@ elevationNormFactors = computeElevationNormFactor(elevations, dElevation, ...
 azimuths = linspace(0,360,nAzimuths+1);
 azimuths = azimuths(1:end-1);
 
-for k = 1:nScales %main loop
-    
+totalOrientationNumber = length(azimuths) * length(elevations) + 1;
+totalIterationNumber = 2 * nScales * totalOrientationNumber;
+
+for k = 1:nScales
     vidS = imresize3(vidIn,[1/k, 1/k, 1/k] .* size(vidIn),'Antialiasing',true);
     vidOriTot_n = zeros(size(vidS));
     vidOriTot_p = zeros(size(vidS));
+    CnArr = zeros([size(vidS), totalOrientationNumber]);
+    CpArr = zeros([size(vidS), totalOrientationNumber]);
     primaryFL = max(3, maxFacilitationLength/k);
-    secondaryFL = max(3, minFacilitationLength/k);    
+    secondaryFL = max(3, minFacilitationLength/k);
     facilitationLengths = computeEllipsoidRadius(elevations, primaryFL, secondaryFL);
     if(minAngle == 0) %0 elev handling
-        [LF_n, LF_p] = Gabor3DActivation(vidS, 0, 0, activationThreshold, primaryFL, alpha);
-       vidOriTot_n = vidOriTot_n+(LF_n*elevationNorm0Factor).^m1;
+        L = BuildGabor3D(0, 0);
+        Co = convn(vidS, L,'same');
+        CpArr(:,:,:,end) = max(Co,0);
+        CnArr(:,:,:,end) = max(-Co,0);
+    end
+    for i = 1:length(azimuths)
+        for j = 1:length(elevations)
+            currOrientationIndex = i * length(elevations) + j;
+            L = BuildGabor3D(azimuths(i), elevations(j));
+            Co = convn(vidS, L,'same');
+            CpArr(:,:,:,currOrientationIndex) = max(Co,0);
+            CnArr(:,:,:,currOrientationIndex) = max(-Co,0);
+            
+            progressCounter = progressCounter + 1;
+            waitbar(progressCounter / totalIterationNumber, w);
+        end
+    end
+    
+    CpTotalPowerSum = sum(abs(CpArr).^normQ, 4);
+    
+    CnTotalPowerSum = sum(abs(CnArr).^normQ, 4);
+    if(minAngle == 0) %0 elev handling
+        %     CpOriSum = sumButIndexPowerNormed(CpArr, totalOrientationNumber, 2);
+        %     CnOriSum = sumButIndexPowerNormed(CnArr, totalOrientationNumber, 2);
+        CpNormFactor = 1 + (CpTotalPowerSum - abs(CpArr(:,:,:, end)).^normQ).^(1/normQ);
+        CnNormFactor = 1 + (CnTotalPowerSum - abs(CnArr(:,:,:, end)).^normQ).^(1/normQ);
+        Cp = CpArr(:,:,:, totalOrientationNumber) ./ CpNormFactor;
+        Cn = CnArr(:,:,:, totalOrientationNumber) ./ CnNormFactor;
+        
+        [LF_n, LF_p] = Gabor3DActivation(Cp,Cn, 0, 0, activationThreshold, primaryFL, alpha);
+        vidOriTot_n = vidOriTot_n+(LF_n*elevationNorm0Factor).^m1;
         vidOriTot_p = vidOriTot_p+(LF_p*elevationNorm0Factor).^m1;
     end
     for i = 1:length(azimuths)
         for j = 1:length(elevations)
-            [LF_n, LF_p] = Gabor3DActivation(vidS, azimuths(i), elevations(j), ...
+            currOrientationIndex = (i-1) * length(elevations) + j;
+            CnNormFactor = 1 + (CnTotalPowerSum - abs(CnArr(:,:,:, currOrientationIndex)).^normQ).^(1/normQ);
+            CpNormFactor = 1 + (CpTotalPowerSum - abs(CpArr(:,:,:, currOrientationIndex)).^normQ).^(1/normQ);
+            
+            Cp = CpArr(:,:,:, currOrientationIndex) ./ CpNormFactor;
+            Cn = CnArr(:,:,:, currOrientationIndex) ./ CnNormFactor;
+            
+            [LF_n, LF_p] = Gabor3DActivation(Cp,Cn, azimuths(i), elevations(j), ...
                 activationThreshold, facilitationLengths(j), alpha);
             %combining angles
-            vidOriTot_n = vidOriTot_n+(LF_n*elevationNormFactors(j)).^m1;
             vidOriTot_p = vidOriTot_p+(LF_p*elevationNormFactors(j)).^m1;
+            vidOriTot_n = vidOriTot_n+(LF_n*elevationNormFactors(j)).^m1;
             %waitbar handling
             progressCounter = progressCounter + 1;
             waitbar(progressCounter / totalIterationNumber, w);
         end
     end
-    %rooting angles
-    vidOriTot_n = vidOriTot_n.^(1/m1);
+    
     vidOriTot_p = vidOriTot_p.^(1/m1);
-    %combining scales
+    vidOriTot_n = vidOriTot_n.^(1/m1);
+    
     vidScaled = imresize3(vidOriTot_p.^m2 - vidOriTot_n.^m2,size(vidIn));
     vidScaled = vidScaled/(k^m2);
     vidScalesPyr{k} = vidScaled;
