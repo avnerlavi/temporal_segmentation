b49dbcc5c43112b0e71ef2dbcb52e32c164e7f8c
Avner Lavi <avnerlavi308@gmail.com>
2022-01-22 18:21:28 +0200

    added usage of LF support in Gabor activation

--- 3dGabor/Gabor3DActivation.m
+++ 3dGabor/Gabor3DActivation.m
@@ -15,15 +15,18 @@ LF_n_mask = LFsc3D_binarized(CnSupport, Azimuth, Elevation ,FacilitationLength, 
 [LF_p ,NR_p] = LFsc3D(Cp, Azimuth, Elevation, FacilitationLength);
 [LF_n ,NR_n] = LFsc3D(Cn, Azimuth, Elevation ,FacilitationLength);
 
-threshold_p = activationThreshold * max(LF_p(8:end-7,8:end-7,8:end-7), [], 'all');
-threshold_n = activationThreshold * max(LF_n(8:end-7,8:end-7,8:end-7), [], 'all');
+LF_p = (LF_p - NR_p) .* LF_p_mask + NR_p;
+LF_n = (LF_n - NR_n) .* LF_n_mask + NR_n;
+
+% threshold_p = activationThreshold * max(LF_p(8:end-7,8:end-7,8:end-7), [], 'all');
+% threshold_n = activationThreshold * max(LF_n(8:end-7,8:end-7,8:end-7), [], 'all');
 
 threshold_data = [Azimuth,Elevation];
 threshold_data(end,4) = mean(double(abs(LF_p) > threshold_p),'all');
 threshold_data(end,3) = mean(double(abs(LF_n) > threshold_n),'all');
 
-LF_p(abs(LF_p) < threshold_p) = NR_p(abs(LF_p) < threshold_p);
-LF_n(abs(LF_n) < threshold_n) = NR_n(abs(LF_n) < threshold_n);
+% LF_p(abs(LF_p) < threshold_p) = NR_p(abs(LF_p) < threshold_p);
+% LF_n(abs(LF_n) < threshold_n) = NR_n(abs(LF_n) < threshold_n);
 
 LF_p=0.5*max(0,LF_p-alpha*NR_p);
 LF_n=0.5*max(0,LF_n-alpha*NR_n);
