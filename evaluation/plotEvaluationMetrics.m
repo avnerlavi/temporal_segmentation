cf56ab2e80dbbfd163227f7a4893837e9635d8e6
Avner Lavi <avnerlavi308@gmail.com>
2022-03-02 17:15:38 +0200

    added datatip marking for top iou in evaluation subplot

--- evaluation/plotEvaluationMetrics.m
+++ evaluation/plotEvaluationMetrics.m
@@ -15,7 +15,7 @@ ylim([0, 1]);
 ylabel('recall');
 
 subplot(2,2,3);
-plot(thresholds, ious);
+iouPlot = plot(thresholds, ious);
 xlim([min(thresholds), max(thresholds)]);
 xlabel('threshold');
 ylim([0, 1]);
@@ -26,7 +26,12 @@ plot(recalls, precisions);
 xlim([0, 1]);
 xlabel('recall');
 ylim([0, 1]);
-ylabel('precision');
+ylabel('precision');    
 
 plotHandle = get(groot,'CurrentFigure');
+[maxIoU, maxIoUIndex] = max(ious);
+cursorMode = datacursormode(plotHandle);
+hDatatip = cursorMode.createDatatip(iouPlot);
+pos = [thresholds(maxIoUIndex) maxIoU 0];
+set(hDatatip, 'Position', pos); 
 end
