b64d656927db79cddef645dd8704e7ae569b4589
Avner Lavi <avnerlavi308@gmail.com>
2022-03-21 19:17:51 +0200

    added function to save snapshots of selected frames

---
+++ utils/saveSnapshots.m
@@ -0,0 +1,34 @@
+function saveSnapshots(vidIn, fileDir, frames)
+if fileDir(end) ~= '\'
+    fileDir = [fileDir, '\'];
+end
+
+mkdir(fileDir);
+
+if ~exist('frames','var')
+    frames = [60, 120];
+end
+
+dims = size(vidIn);
+frameCount = dims(end);
+otherdims = repmat({':'},1,ndims(vidIn)-1);
+for i=1:length(frames)
+    if (frames(i) > frameCount)
+        disp(['frame ', frames(i), ' is out of range']);
+        return;
+    end
+    
+    frame = vidIn(otherdims{:}, frames(i));
+    hFig = figure;
+    set(hFig, 'color', 'white');
+    imagesc(frame);
+    axis off;
+    colorbar;
+    colormap gray;
+    set(gca,'position',[0.05 0.05 0.82 0.9]);
+    [cdata, ~] = getframe(hFig);
+    imwrite(cdata, [fileDir,'\frame',num2str(frames(i)),'.jpg']);
+    close(hFig);
+end   
+
+end
