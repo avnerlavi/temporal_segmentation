f9a64be5fc69b8e36348dc92cc6ba6e9eed60816
Avner Lavi <avnerlavi308@gmail.com>
2022-04-30 15:19:58 +0300

    added std feature rectification

--- 3dGaussianStd/computeCombinedStd_IN3D.m
+++ 3dGaussianStd/computeCombinedStd_IN3D.m
@@ -25,14 +25,14 @@ for k = 1:nScales
     %% total STD difference power norm caclculation
     %0 elev handling
     temporalStd = Std3DActivation(spatialStd, sigmaTemporal, 0, 0);
-    stdDiff = minMaxNorm(spatialStd) - minMaxNorm(temporalStd);
+    stdDiff = max(minMaxNorm(spatialStd) - minMaxNorm(temporalStd), 0);
     stdTotalPowerSum = abs(stdDiff).^normQ;
     
     if k == 1 || k == 2
         saveSnapshots(gather(spatialStd(relativePaddingSize+1:end-relativePaddingSize, ...
             relativePaddingSize+1:end-relativePaddingSize, :)), snapshotDir, ['spatial_std_k_', num2str(k)], frames);
         saveSnapshots(gather(temporalStd(relativePaddingSize+1:end-relativePaddingSize, ...
-            relativePaddingSize+1:end-relativePaddingSize, :)), snapshotDir, ['temporal_std_k_', num2str(k)], frames);
+            relativePaddingSize+1:end-relativePaddingSize, :)), snapshotDir, ['temporal_std_k_', num2str(k), '_az_0_el_0'], frames);
     end
     
     progressCounter = progressCounter + 1;
@@ -41,7 +41,7 @@ for k = 1:nScales
     for i = 1:length(azimuths)
         for j = 1:length(elevations)
             temporalStd = Std3DActivation(spatialStd, sigmaTemporal, azimuths(i), elevations(j));
-            stdDiff = minMaxNorm(spatialStd) - minMaxNorm(temporalStd);
+            stdDiff = max(minMaxNorm(spatialStd) - minMaxNorm(temporalStd), 0);
             stdTotalPowerSum = stdTotalPowerSum + abs(stdDiff).^normQ;
             
             progressCounter = progressCounter + 1;
@@ -52,16 +52,16 @@ for k = 1:nScales
     %% STD difference normalization
     %0 elev handling
     temporalStd = Std3DActivation(spatialStd, sigmaTemporal, 0, 0);
-    stdDiff = minMaxNorm(spatialStd) - minMaxNorm(temporalStd);
+    stdDiff = max(minMaxNorm(spatialStd) - minMaxNorm(temporalStd), 0);
     stdDiffNormFactor = 1 + (stdTotalPowerSum - abs(stdDiff).^normQ).^(1/normQ);
     stdDiffNormed = stdDiff ./ stdDiffNormFactor;
     stdDiffOriTot = sign(stdDiffNormed) .* (abs(stdDiffNormed).^m1);
     
     if k == 1 || k == 2
         saveSnapshots(gather(stdDiff(relativePaddingSize+1:end-relativePaddingSize, ...
-            relativePaddingSize+1:end-relativePaddingSize, :)), snapshotDir, ['std_diff_before_norm_k_', num2str(k)], frames);
+            relativePaddingSize+1:end-relativePaddingSize, :)), snapshotDir, ['std_diff_before_norm_k_', num2str(k), '_az_0_el_0'], frames);
         saveSnapshots(gather(stdDiffNormed(relativePaddingSize+1:end-relativePaddingSize, ...
-            relativePaddingSize+1:end-relativePaddingSize, :)), snapshotDir, ['std_diff_after_norm_k_', num2str(k)], frames);
+            relativePaddingSize+1:end-relativePaddingSize, :)), snapshotDir, ['std_diff_after_norm_k_', num2str(k), '_az_0_el_0'], frames);
     end
     
     progressCounter = progressCounter + 1;
@@ -70,10 +70,15 @@ for k = 1:nScales
     for i = 1:length(azimuths)
         for j = 1:length(elevations)
             temporalStd = Std3DActivation(spatialStd, sigmaTemporal, azimuths(i), elevations(j));
-            stdDiff = minMaxNorm(spatialStd) - minMaxNorm(temporalStd);
+            stdDiff = max(minMaxNorm(spatialStd) - minMaxNorm(temporalStd), 0);
             stdDiffNormFactor = 1 + (stdTotalPowerSum - abs(stdDiff).^normQ).^(1/normQ);
             stdDiffNormed = stdDiff ./ stdDiffNormFactor;
             stdDiffOriTot = stdDiffOriTot + sign(stdDiffNormed) .* (abs(stdDiffNormed).^m1);
+            
+            if azimuths(i) == 90 && j == length(elevations) && k == 1
+                saveSnapshots(gather(stdDiffNormed(relativePaddingSize+1:end-relativePaddingSize, ...
+                    relativePaddingSize+1:end-relativePaddingSize, :)), snapshotDir, ['std_diff_after_norm_k_', num2str(k), '_az_', num2str(azimuths(i)), '_el_', num2str(elevations(j))], frames);
+            end
             
             progressCounter = progressCounter + 1;
             waitbar(progressCounter / totalIterationNumber, w);   
