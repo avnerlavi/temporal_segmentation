5272d38c54978e0059983fa35d27208f22e9fa76
Galalon <sgalalon@gmail.com>
2022-09-16 18:28:11 +0300

    linear stitch and gamma prototype

--- 3dGabor/additiveCombination.m
+++ 3dGabor/additiveCombination.m
@@ -1,7 +1,14 @@
 function [vidOut] = additiveCombination(vid_source, vid_additive, beta, gain)
 
-additive_p = (max(vid_additive, 0))./(1+beta*vid_source);
-additive_n = (max(-vid_additive, 0))./(1+beta*(1-vid_source));
+beta_x_const = (beta - 1) ./ (2*beta);
+beta_y_const =  4*beta ./ (1 + beta).^2;
+additive_p = (max(vid_additive, 0)) ./ (1 + beta*vid_source);
+additive_p(vid_source >= beta_x_const) = beta_y_const .* max(vid_additive(vid_source >= beta_x_const), 0) .*...
+    (1 - vid_source(vid_source >= beta_x_const));
+additive_n = (max( -vid_additive, 0))./(1 + beta*(1 - vid_source));
+additive_n((1 - vid_source) >= beta_x_const) = beta_y_const .* max(-vid_additive((1 - vid_source) >= beta_x_const), 0) .*...
+    (vid_source((1 - vid_source) >= beta_x_const));
+
 vidOut = vid_source + gain * (additive_p - additive_n);
 
 end
