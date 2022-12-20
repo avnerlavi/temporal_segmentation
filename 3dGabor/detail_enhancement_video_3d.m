6bbd5df1b2a386b4309a1856275f9d0da98155f7
Galalon <sgalalon@gmail.com>
2022-12-21 00:48:44 +0200

    changing plot orientation data

--- 3dGabor/detail_enhancement_video_3d.m
+++ 3dGabor/detail_enhancement_video_3d.m
@@ -9,7 +9,8 @@ elevationHalfAngle = [0, 60];
 resizeFactors = [1, 1, 1];
 %[root ,'/resources/material_from_ynon_19_1_22/filtered_new/cropped/heart_malformation/2805_2818.avi'];
 %"F:\Matlab\docs\temporal_segmentation\resources\material_from_ynon_19_1_22\filtered_new\cropped\eve_c_c\0258_0352_chunk_1.avi.avi"
-inFileDir = [root ,'/resources/material_from_ynon_19_1_22/filtered_new/cropped/eye_c_c/0258_0352_chunk_1.avi.avi'];
+%"F:\Matlab\docs\temporal_segmentation\resources\ImpulseCheckingVids\movingDot.avi"
+inFileDir = [root ,'/resources/þþImpulseCheckingVids/movingLine.avi'];
 %%
 if(generatePyrFlag)
     inFileDir = [root,'/captcha_running.avi'];
@@ -17,20 +18,19 @@ if(generatePyrFlag)
     vid_matrix = imresize(vid_matrix_orig, 0.25);
     [vid_matrix] = StdUsingPyramidFunc(vid_matrix);
 else
-    vid_matrix = readVideoFromFile(inFileDir, false,[1200,1558]);
-    vid_matrix =convn(vid_matrix,1/3*ones(1,1,3),'valid');
+    vid_matrix = readVideoFromFile(inFileDir, false);
+    %vid_matrix =convn(vid_matrix,1/3*ones(1,1,3),'valid');
 end
-vid_matrix = vid_matrix(:,:,60:end);
 vid_matrix = safeResize(vid_matrix, resizeFactors.*size(vid_matrix));
 
 CCLFParams = struct;
-CCLFParams.numOfScales = 4;
+CCLFParams.numOfScales = 1;
 CCLFParams.elevationHalfAngle = atand(tand(elevationHalfAngle) * resizeFactors(1) / resizeFactors(3));
-CCLFParams.azimuthNum = 8;
-CCLFParams.elevationNum = 6;
+CCLFParams.azimuthNum = 36;
+CCLFParams.elevationNum = 10;
 CCLFParams.eccentricity = sqrt(1);
 CCLFParams.activationThreshold = 0.12; %for running man - 0.3
-CCLFParams.facilitationLengths = [5, 5];
+CCLFParams.facilitationLengths = 5;
 CCLFParams.alpha = 0.5;
 CCLFParams.m1 = 1;
 CCLFParams.m2 = 2;
